c5936a68ad8da6071c6e1f9451423c7a22c5d6d2
LorinceTawamba <lorince.tawamba@gmail.com>
2024-10-25 18:15:03 -0400

    Update file

---
+++ fullstack/formation/html-css-javascript/scripts/operation.js
@@ -0,0 +1,18 @@
+console.log('Formation Javascript');
+
+let age = 25; 
+console.log(age); 
+
+let poids = 18.35;
+console.log(poids);
+
+const PI = 22/7; 
+console.log(PI); 
+
+console.log(typeof age, age); 
+
+let test = false; 
+console.log(typeof test, test); 
+
+let numbers = [22, 68, 79, 45];
+console.log(typeof numbers, numbers);
